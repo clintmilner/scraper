55d19efd3ed8bad92386c47a757b19398b4db3b2
Clint Milner <clinton.milner@rebasoft.net>
2019-09-10 17:32:54 +0100

    code clean-ups

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import db from './lib/db';
 import './lib/cron';
 import {getInstagramCount, getTwitterCount} from './lib/scraper';
 
@@ -7,10 +6,13 @@ const PORT = 9999;
 const app = express();
 
 app.get('/scrape', async (req, res, next) => {
-    console.log('getting all the data');
     const [twitterTweetCount, twitterFollowingCount, twitterFollowerCount, twitterLikeCount, instagramPostCount, instagramFollowingCount, instagramFollowerCount] = await Promise.all([getTwitterCount('tweets'), getTwitterCount('following'), getTwitterCount('followers'), getTwitterCount('favorites'), getInstagramCount('posts'), getInstagramCount('following'), getInstagramCount('followers')]);
+
+    // we won't write to the file if we're just hitting the endpoint...
     // db.get('twitter').push({ timestamp, twitterTweetCount, twitterFollowingCount, twitterFollowerCount, twitterLikeCount }).write();
     // db.get('instagram').push({ timestamp, instagramPostCount, instagramFollowingCount, instagramFollowerCount }).write();
+
+    console.log('returning the latest data');
     res.json({
         twitterTweetCount,
         twitterFollowerCount,
