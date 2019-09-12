fe15274358d56ed54cb184af71a0953a27d280df
Clint Milner <clinton.milner@rebasoft.net>
2019-09-12 14:50:08 +0100

    fixing just for twitter

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.get('/scrape', async (req, res, next) => {
     // const [twitterTweetCount, twitterFollowingCount, twitterFollowerCount, twitterLikeCount, instagramPostCount, instagramFollowingCount, instagramFollowerCount] = await Promise.all([getTwitterCount('tweets'), getTwitterCount('following'), getTwitterCount('followers'), getTwitterCount('favorites'), getInstagramCount('posts'), getInstagramCount('following'), getInstagramCount('followers')]);
-    const [instagramPostCount] = await Promise.all([getInstagramCount('followers')]);
+    const [twitterTweetCount, twitterFollowingCount, twitterFollowerCount, twitterLikeCount] = await Promise.all([getTwitterCount('tweets'), getTwitterCount('following'), getTwitterCount('followers'), getTwitterCount('favorites')]);
+    // const [instagramPostCount] = await Promise.all([getInstagramCount('followers')]);
 
     // we won't write to the file if we're just hitting the endpoint...
     // db.get('twitter').push({ timestamp, twitterTweetCount, twitterFollowingCount, twitterFollowerCount, twitterLikeCount }).write();
@@ -15,11 +16,11 @@ app.get('/scrape', async (req, res, next) => {
 
     console.log('returning the latest data');
     res.json({
-        // twitterTweetCount,
-        // twitterFollowerCount,
-        // twitterFollowingCount,
-        // twitterLikeCount,
-        instagramPostCount,
+        twitterTweetCount,
+        twitterFollowerCount,
+        twitterFollowingCount,
+        twitterLikeCount,
+        // instagramPostCount,
         // instagramFollowingCount,
         // instagramFollowerCount
     })
